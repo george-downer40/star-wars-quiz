f4a46cefb18159766045d89922c16be37935826b
george-downer40 <george.downer40@gmail.com>
2022-03-21 18:41:59 +0000

    add question variable in js script

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let questions = [
+    {
+        question: 'Who destroyed the first Death Star?',
+        answers: [
+            {text: 'Luke Skywalker', correct: true},
+            {text: 'Wrong', correct: false},
+            {text: 'Wrong', correct: false},
+            {text: 'Wrong', correct: false}
+        ]
+    }
+];
