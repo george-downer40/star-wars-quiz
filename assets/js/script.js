a14ec7b428e9115233ca5c26d201f822179dadf2
george-downer40 <george.downer40@gmail.com>
2022-03-22 01:10:12 +0000

    update questions in questions variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,36 @@
+console.log('working?');
+
+
 let questions = [
     {
         question: 'Who destroyed the first Death Star?',
-        answers: [
-            {text: 'Luke Skywalker', correct: true},
-            {text: 'Wrong', correct: false},
-            {text: 'Wrong', correct: false},
-            {text: 'Wrong', correct: false}
-        ]
-    }
+        choice1: 'Luke Skywalker',
+        choice2: 'Wrong',
+        choice3: 'Wrong',
+        choice4: 'Wrong',
+        answer: 1,
+    },
+    {
+        question: 'Who dueled Darth Sidious in the Senate on Coruscant?',
+        choice1: 'Wrong',
+        choice2: 'Yoda',
+        choice3: 'Wrong',
+        choice4: 'Wrong',
+        answer: 2,
+    },
+    {
+        question: 'Who was the apprentice of Qui-Gon Jinn?',
+        choice1: 'Wrong',
+        choice2: 'Wrong',
+        choice3: 'Obi-Wan Kenobi',
+        choice4: 'Wrong',
+        answer: 3,
+    },
 ];
 
 let questionBox = document.getElementById('question-box');
+
+function displayQuestion(questions) {
+    console.log('working?');
+}
+    
